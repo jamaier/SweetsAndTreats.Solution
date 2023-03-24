7017736e2db53d846de50f2ad6787cffaa039afd
Jacob Maier <jacobamaier@gmail.com>
2023-03-24 09:22:20 -0700

    Add data annotation and login/register viewModels.

--- SweetsAndTreats/Models/Flavor.cs
+++ SweetsAndTreats/Models/Flavor.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SweetsAndTreats.Models.Models
 {
   public class Flavor
   {
     public int FlavorId { get; set; }
+    [Required(ErrorMessage = "You must enter a flavor")]
     public string FlavorName { get; set; }
     public List<FlavorTreat> JoinEntities { get; set; }
   }
--- SweetsAndTreats/Models/Treat.cs
+++ SweetsAndTreats/Models/Treat.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SweetsAndTreats.Models
 {
   public class Treat
   {
     public int TreatId { get; set; }
+    [Required(ErrorMessage = "You must enter a treat")]
     public string TreatName { get; set; }
-    public List<FlavorTreat> JoinEntities { get; set;}
+    public List<FlavorTreat> JoinEntities { get; set; }
   }
 }
